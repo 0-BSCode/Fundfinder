7a3df23d993499d7131883019c459c4c53689712
Bryan Sanchez <bryanipsanchez@gmail.com>
2022-08-16 19:34:18 +0800

    Log in reflects on display

--- client/src/components/navbar/index.tsx
+++ client/src/components/navbar/index.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement, useContext } from "react";
 import { CrowdfundContext } from "src/context/CrowdfundContext";
 import logo from "public/assets/images/fundfinder-logo.svg";
+import profile from "public/assets/images/profile-sample.png";
 import styles from "./styles.module.css";
 
 const Navbar = (): ReactElement => {
@@ -9,15 +10,25 @@ const Navbar = (): ReactElement => {
   return (
     <nav className={styles.nav}>
       <img className={styles.nav__logo} src={logo.src} alt={"Fundfinder"} />
-      <button
-        onClick={(e) => {
-          e.preventDefault();
-          connectWallet();
-        }}
-        className={styles.nav__btn}
-      >
-        Get started
-      </button>
+      {currentUser?.id ? (
+        <button className={styles.nav__profile}>
+          <img
+            className={styles.nav__profilePic}
+            src={profile.src}
+            alt={"Profile picture"}
+          />
+        </button>
+      ) : (
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            connectWallet();
+          }}
+          className={styles.nav__btn}
+        >
+          Get started
+        </button>
+      )}
     </nav>
   );
 };
