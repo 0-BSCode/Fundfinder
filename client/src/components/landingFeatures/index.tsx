1df5cb934d681e4483a43442040717a4cbd17a21
Bryan Sanchez <bryanipsanchez@gmail.com>
2022-08-16 21:41:48 +0800

    Desktop design for landing page

--- client/src/components/landingFeatures/index.tsx
+++ client/src/components/landingFeatures/index.tsx
@@ -10,27 +10,29 @@ const LandingFeatures = (): ReactElement => {
   return (
     <section className={styles.features}>
       <h3 className={styles.features__title}>What we make possible</h3>
-      <LandingFeature
-        logo={paymentLogo}
-        feature={"Efficient payment processing"}
-        description={
-          "Leverage the power of blockchain technology to instantly send and receive funds from others with negligible transactional costs."
-        }
-      />
-      <LandingFeature
-        logo={goalLogo}
-        feature={"Quick goal-setting"}
-        description={
-          "Quickly set up the details of the goals you have in mind with just a few clicks."
-        }
-      />
-      <LandingFeature
-        logo={networkingLogo}
-        feature={"Peer-to-peer networking"}
-        description={
-          "Eliminate the need for large financial institutions to be an intermediary between you and the people you transfer funds with using blockchain’s p2p network."
-        }
-      />
+      <div className={styles.features__container}>
+        <LandingFeature
+          logo={paymentLogo}
+          feature={"Efficient payment processing"}
+          description={
+            "Leverage the power of blockchain technology to instantly send and receive funds from others with negligible transactional costs."
+          }
+        />
+        <LandingFeature
+          logo={goalLogo}
+          feature={"Quick goal-setting"}
+          description={
+            "Quickly set up the details of the goals you have in mind with just a few clicks."
+          }
+        />
+        <LandingFeature
+          logo={networkingLogo}
+          feature={"Peer-to-peer networking"}
+          description={
+            "Eliminate the need for large financial institutions to be an intermediary between you and the people you transfer funds with using blockchain’s p2p network."
+          }
+        />
+      </div>
       <LandingDivider />
     </section>
   );
